6db85832ef863a98704c5803e015efb97b23c309
123 <1@localhost.localdomain>
2017-11-20 16:41:39 +0800

    是一个求100以内的奇数的函数。

---
+++ test/2.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 2.c
+	> Author: linuxidc
+	> Mail: linuxidc@linuxidc.com 
+	> Created Time: 2017年11月20日 星期一 16时36分38秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(viod)
+{
+	int i=0;
+	for(i=0;i<100;i++)
+	{
+		if(i%2)
+			printf("%d\t",i);
+	}
+	return 0;
+}
